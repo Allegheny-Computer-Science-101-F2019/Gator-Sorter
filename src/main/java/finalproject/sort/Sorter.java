78c7c6eaf08094747f38c7c327e5602d698a154d
Pedro Carmo <carmop@aldenv169.allegheny.edu>
2019-12-05 15:46:29 -0500

    Added sortToTime()

--- src/main/java/finalproject/sort/Sorter.java
+++ src/main/java/finalproject/sort/Sorter.java
@@ -30,7 +30,7 @@ public abstract class Sorter {
   /** Require that a Sorter provide a method for int[] arrays. */
   public abstract int[] sort(int[] source);
 
-  /** TODO: add a Sorter and Timer method */
+  /** TODO: add a Sorter and   Timer method */
   public long sortToTime(int[] source) {
     long startTime = System.currentTimeMillis();
     sort(source);
