7ed26ae3fe608f03b1dca8b84cf84e2afa4e2283
noorbuchi <buchin@allegheny.edu>
2019-12-05 12:06:41 -0500

    Added MainApp.java and added methods to selection sort. need implemented

---
+++ src/main/java/finalProject/application/MainApp.java
@@ -0,0 +1,26 @@
+package finalProject.application;
+
+import finalProject.sort.BubbleSort;
+import finalProject.sort.InsertionSort;
+import finalProject.sort.SelectionSort;
+import finalProject.sort.Sorter;
+
+// Done: Add all of the needed comments to this file
+/**
+ * Main method creates RunCampaign,runs both bubbleSort and insertionSort in it.
+ * and adds the result to the respecteive ResultsTable.
+ *
+ * @author Gregory M. Kapfhammer @author Noor Buchi
+ */
+public class MainApp {
+
+  /**
+   * main methd receives no arguments.
+   * used to create necessayry variables, call appropriate methods and update
+   * output.
+   */
+  public static void main(String[] args) {
+    
+  }
+
+}
