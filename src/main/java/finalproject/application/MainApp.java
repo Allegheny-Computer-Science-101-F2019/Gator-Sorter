18531f68c3fc64c1825f6e51d42df7e36583311b
noorbuchi <buchin@allegheny.edu>
2019-12-09 20:11:46 -0500

    added comments for main

--- src/main/java/finalproject/application/MainApp.java
+++ src/main/java/finalproject/application/MainApp.java
@@ -40,6 +40,7 @@ public class MainApp {
    * output.
    */
   public static void main(String[] args) {
+    System.out.println();
     System.out.println("Welcome to GatorSorter!");
     System.out.println();
     System.out.println(
@@ -47,19 +48,20 @@ public class MainApp {
     System.out.println();
     Scanner userInput = new Scanner(System.in);
     int dataSize = 0;
-    while (dataSize < 5 || dataSize > 20) {
-      System.out.println(
-          "Please enter the size of the data structure to be generated ");
-      System.out.print("(Size must be between 5 and 20)");
+    while (dataSize < 5 || dataSize > 20) { // input must be between 5 and 20
+      System.out.println("Please enter the size of the data structure to be generated "
+          + "(Size must be between 5 and 20)");
       dataSize = userInput.nextInt();
     }
     System.out.println("Chosen size of array: " + dataSize);
     int[] originalArray = generateRandomArray(dataSize);
+    // display generated
     System.out.println("Genereated Array: " + Arrays.toString(originalArray));
     int[] copiedArray = new int[dataSize];
+    // makes copy of array to send into efficientSort
     System.arraycopy(originalArray, 0, copiedArray, 0, dataSize);
     int sortselect = 0;
-    while (sortselect < 1 || sortselect > 3) {
+    while (sortselect < 1 || sortselect > 3) { // input must be between 1 and 3
       System.out.println("Please enter which sorting algorithm you wish to use:\n");
       System.out.println("For BubbleSort enter 1 ");
       System.out.println("For InsertionSort enter 2");
@@ -67,32 +69,35 @@ public class MainApp {
       sortselect = userInput.nextInt();
     }
     if (sortselect == 1) {
-      //Bubblesort
+      //Bubblesort is selected
       BubbleSort bubbleSorter = new BubbleSort();
       System.out.println("You selected the BubbleSort");
+      // sort() is responsible for printing out information
       bubbleSorter.sort(originalArray);
       System.out.println();
       System.out.println("Time taken to sort using " + bubbleSorter.getName()
                 + ": " + bubbleSorter.sortToTime(copiedArray) + " nanoseconds");
-      bubbleSorter.displayInfo();
+      bubbleSorter.displayInfo(); //calls displayInfo for bubbleSorter
     } else if (sortselect == 2) {
-      //InsertionSort
+      //InsertionSort is selected
       InsertionSort insertionSorter = new InsertionSort();
       System.out.println("You selected the InsertionSort");
+      // sort() is responsible for printing out information
       insertionSorter.sort(originalArray);
       System.out.println();
       System.out.println("Time taken to sort using " + insertionSorter.getName()
                 + ": " + insertionSorter.sortToTime(copiedArray) + " nanoseconds");
-      insertionSorter.displayInfo();
+      insertionSorter.displayInfo(); //calls displayInfo for insertionSorter
     } else if (sortselect == 3) {
-      //SelectionSort
+      //SelectionSort is selected
       SelectionSort selectionSorter = new SelectionSort();
       System.out.println("You selected the SelectionSort");
+      // sort() is responsible for printing out information
       selectionSorter.sort(originalArray);
       System.out.println();
       System.out.println("Time taken to sort using " + selectionSorter.getName()
                 + ": " + selectionSorter.sortToTime(copiedArray) + " nanoseconds");
-      selectionSorter.displayInfo();
+      selectionSorter.displayInfo(); //calls displayInfo for selectionSorter
     }
   }
 }
